de78496b5c36084696f6139b81d65cd98565219d
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-25 22:33:05 +0000

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,13 @@
+import Button from '@/app/somecomponents/Button'
 export default function Hero(){
     return (
         <div className="containers max-w-screen-xl h-[100vh] w-full flex flex-col justify-center items-center">
-            <div className="">
-            <h1 className="text-center text-[6rem] max-[600px]:text-[2.5rem] font-bold sm:leading-none leading-[0.8]">We build brands, <br /> we create experiences.</h1>
-            <p className="m-auto text-center lg:text-[1.2rem] max-w-screen-md mt-[1rem] tex-neutral-300">We're a full-service digital marketing and web development agency that specializes in building brands and creating experiences</p>
+            <div className="flex flex-col items-center justify-center gap-4">
+                <h1 className="text-center text-[6rem] max-[600px]:text-[2.5rem] font-bold sm:leading-none leading-[0.8]">We build brands, <br /> we create experiences.</h1>
+                <p className="m-auto text-center lg:text-[1.2rem] max-w-screen-md mt-[1rem] tex-neutral-300">We're a full-service digital marketing and web development agency that specializes in building brands and creating experiences</p>
+                <div>
+                    <a href="#" className="bg-gradient-to-br from-transparent to-slate-800 p-5 border border-slate-600 rounded-full">Get Started</a>
+                </div>
             </div>
         </div>
     )
