03f2069d468333e9499a6a68ed65452d738f89a1
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-25 22:47:07 +0000

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,8 +5,9 @@ export default function Hero(){
             <div className="flex flex-col items-center justify-center gap-4">
                 <h1 className="text-center text-[6rem] max-[600px]:text-[2.5rem] font-bold sm:leading-none leading-[0.8]">We build brands, <br /> we create experiences.</h1>
                 <p className="m-auto text-center lg:text-[1.2rem] max-w-screen-md mt-[1rem] tex-neutral-300">We're a full-service digital marketing and web development agency that specializes in building brands and creating experiences</p>
-                <div>
+                <div className='flex gap-3'>
                     <a href="#" className="bg-gradient-to-br from-transparent to-slate-800 p-5 border border-slate-600 rounded-full">Get Started</a>
+                    <a href="#" className="bg-gradient-to-br from-transparent to-slate-800 p-5 border border-slate-600 rounded-full">Our Projects</a>
                 </div>
             </div>
         </div>
