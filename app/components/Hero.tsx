0a3f1e64e5038fdc43d7f9e48600db877692ebd7
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-26 02:50:59 +0000

    Update Hero.tsx and globals.css

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,8 +2,8 @@ import Button from '@/app/somecomponents/Button'
 import Link from 'next/link'
 export default function Hero(){
     return (
-        <div className="containers max-w-screen-xl h-[100vh] w-full flex flex-col justify-center items-center">
-            <div className="flex flex-col items-center justify-center gap-6">
+        <div className="containers max-w-screen-xl h-[100vh] w-full flexCenter">
+            <div className="flexColCenter gap-6">
                 <h1 className="text-center text-[6rem] max-[600px]:text-[2.5rem] font-bold sm:leading-none leading-[0.8]">We build brands, <br /> we create experiences.</h1>
                 <p className="m-auto text-center lg:text-[1.2rem] max-w-screen-md mt-[1rem] tex-neutral-300">We're a full-service digital marketing and web development agency that specializes in building brands and creating experiences</p>
                 <div className='flex items-center gap-3'>
