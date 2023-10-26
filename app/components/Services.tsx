8c2f7f66d2046bf0742bd9552b88a469bfa057b1
coihsan <110887888+coihsan@users.noreply.github.com>
2023-10-26 03:11:25 +0000

    Update Services.tsx and index.tsx

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import {servicesData} from "@/app/constants/index";
+import Link from 'next/link'
 export default function Services(){
     return(
         <section className="containers max-w-screen-xl">
-            <h1 className="text-5xl font-bold">Our specialisation.</h1>
+            <h1 className="text-5xl font-bold">Our Specialisation.</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
             <div className="grid grid-cols-3 gap-3 max-[600px]:grid-cols-1 ">
                 {servicesData.map((servicesData) =>(
@@ -11,6 +12,7 @@ export default function Services(){
                         <div className="fill-neutral-50 text-[3rem]">{servicesData.icon}</div>
                         <h2 className="text-[2rem] font-bold leading-none">{servicesData.title}</h2>
                         <p>{servicesData.desc}</p>
+                        <Link className="" href={servicesData.url}>Readmore</Link>
                     </div>
                 ))}
             </div>
