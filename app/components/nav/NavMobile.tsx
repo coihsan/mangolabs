e337db54c199a27738cb891180325d786a6a046a
coihsan <ihsan.onlinefs@gmail.com>
2023-11-06 13:55:16 +0700

    commit

--- app/components/nav/NavMobile.tsx
+++ app/components/nav/NavMobile.tsx
@@ -8,11 +8,11 @@ import Link from 'next/link'
     setIsOpen(!isOpen);
   };
     return (
-        <div className='relative w-[30px] h-[30px] flexCenter'>
-            <input type="checkbox" name="" id="" onClick={handleDrawer} className='w-[30px] h-[30px] cursor-pointer z-10 opacity-[0]'/>
-            <div className="absolute right-0 flex flex-col justify-between w-[30px] h-[10px]">
-                <span className="h-[3px] w-full bg-red-500"></span>
-                <span className="h-[3px] w-full bg-red-500"></span>
+        <div className='relative w-[30px] h-[30px] hidden max-[600px]:flexCenter'>
+            <input type="checkbox" name="" id="" onClick={handleDrawer} className='checkbox w-[30px] h-[30px] cursor-pointer z-10 opacity-[0]'/>
+            <div className="hamburger absolute right-0 flex flex-col justify-between w-[30px] h-[13px]">
+                <span className="line1 h-[2px] w-full bg-zinc-50 rounded-full"></span>
+                <span className="line2 h-[2px] w-full bg-zinc-50 rounded-full"></span>
             </div>
         </div>
     )
@@ -45,19 +45,19 @@ export const NavbarResp = () =>{
       }, [isOpen]);
 
     return(
-        <div className='absolute top-0 mx-auto w-full max-[600px]:fixed max-[600px]:bg-black/10 max-[600px]:backdro-blur-xl z-50'>
+        <div className='absolute top-0 mx-auto w-full max-[600px]:fixed max-[600px]:bg-black/30 max-[600px]:backdrop-blur-xl z-50'>
           <div className="mx-auto flex items-center justify-between max-[600px]:w-full  max-w-screen-xl px-4 py-12 max-[600px]:py-4">
           <Link href="#" className="">
             Mango Labs
           </Link>
-          <div className="hidden flex-nowrap max-[600px]:w-full lg:flex items-center transition-all h-full">
-            <div className="p-3">
+          <div className=" flex-nowrap max-[600px]:w-full lg:flex items-center transition-all h-full">
+            <div className="hidden lg:flex p-3">
               {NAV_LINK.map((link) =>(
                   <Link className="align-baseline hover:bg-indigo-950 hover:text-indigo-400 hover:font-bold transition-all duration-300 ease-in-out px-4 py-2 hover:rounded-full" href={link.href} key={link.key}>{link.label}</Link>
                 ))}
             </div>
-            <HamburgerMenu />
           </div>
+            <HamburgerMenu />
         </div>
         </div>
     )
