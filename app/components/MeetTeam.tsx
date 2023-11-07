4c9aeb46152d2740694ae0ac791ecdae68665e9d
M.Ihsan <110887888+coihsan@users.noreply.github.com>
2023-11-07 20:27:37 +0700

    MeetTeam.tsx

---
+++ app/components/MeetTeam.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function MeetTeam(){
+    return (
+        <section className="containers max-w-screen-xl">
+            <div className="py-[10rem] max-[600px]:py-[5rem] w-[100%]">
+                <h1 className="text-5xl title-style text-center">Meet Our Team</h1>
+                <div className="grid">
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
