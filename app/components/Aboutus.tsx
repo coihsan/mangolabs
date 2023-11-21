c4fd04c93d28be3564730ff22f21b5a81e71823f
coihsan <110887888+coihsan@users.noreply.github.com>
2023-11-21 16:35:41 +0000

    Update Aboutus.tsx and Hero.tsx

--- app/components/Aboutus.tsx
+++ app/components/Aboutus.tsx
@@ -1,9 +1,9 @@
 export default function AboutUs(){
     return(
-        <section className="max-w-screen-xl mx-auto py-[7rem] px-[1rem] ">
-            <h1 className="text-8xl title-style flex-1">We Make Good Results</h1>
-            <div className="flex-1 flex flex-col ">
-                <h2 className="text-[2rem] leading-none">Mango Labs is a multidisciplinary creative studio at the intersection of art, design and technology.</h2>
+        <section className="max-w-screen-sm mx-auto py-[7rem] px-[1rem] flex flex-col gap-3">
+            <h1 className="text-8xl max-[600px]:text-6xl title-style flex-1">We Make Good Results</h1>
+            <div className="flex-1 flex flex-col gap-3">
+                <h2 className="max-[600px]:text-[1.5rem] text-[2rem] leading-none">Mango Labs is a multidisciplinary creative studio at the intersection of art, design and technology.</h2>
                 <p className="">Our goal is to deliver amazing experiences that make people talk, and build strategic value for brands, tech, entertainment, arts & culture.</p>
             </div>
         </section>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -31,8 +31,8 @@ export default function Hero(){
                 transition={{ delay: 0.6 }}
                 className="m-auto text-center lg:text-[1.2rem] max-w-screen-md mt-[1rem] tex-neutral-300 max-[470px]:text-[14px]">We're a full-service digital marketing and web development agency that specializes in building brands and creating experiences</motion.p>
                 <div className='grid grid-cols-2 justify-center items-center gap-3 pt-[2rem] max-[600px]:w-full'>
-                    <Link  href="#" className="text-center btn-primary btn-rules box-shadow-hover bg-sky-500">Our Projects</Link>
-                    <Link  href="#" className="text-center btn-primary btn-rules box-shadow-hover">Contact Us</Link>
+                    <Link  href="#" className="text-center btn-primary btn-rules box-shadow-hover bg-sky-500">Contact Us </Link>
+                    <Link  href="#" className="text-center btn-primary btn-rules box-shadow-hover">Our Projects</Link>
                 </div>
                 
             </div>
