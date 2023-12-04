fea74411bd35e50f53da88d641afd28dc06227ea
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-04 13:48:41 +0000

    Update 6 files

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,31 +1,51 @@
 import Image from "next/image";
-import {servicesData} from "@/app/constants/index";
-import Link from 'next/link'
-import { inView } from "framer-motion"
-export default function Services(){
-    return(
-        <section className="bgGrid w-full h-full">
-            <div className="containers max-w-screen-xl ">
-            <div className="py-[10rem] max-[600px]:py-[5rem] w-[100%]">
-                <div className="flexColCenter gap-2 mb-[4rem]">
-                    <h1 className="font-bold textTitle title-style">Our Specialisation.</h1>
-                    <p className="max-[600px]:text-center text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+import { servicesData } from "@/app/constants/index";
+import Link from "next/link";
+import { inView } from "framer-motion";
+export default function Services() {
+  return (
+    <section className="bgGrid w-full h-full">
+      <div className="containers max-w-screen-xl ">
+        <div className="py-[10rem] max-[600px]:py-[5rem] w-[100%]">
+          <div className="flexColCenter gap-2 mb-[4rem]">
+            <h1 className="font-bold textTitle title-style">
+              Our Specialisation.
+            </h1>
+            <p className="max-[600px]:text-center text-zinc-300">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <div className="grid grid-cols-3 w-[100%] max-[600px]:grid-cols-1 gap-5 ">
+            {servicesData.map((servicesData) => (
+              <Link
+                href={servicesData.url}
+                key={servicesData.id}
+                className="relative group flex flex-col bg-gradient-to-tl from-zinc-900 to-transparent border border-zinc-50/20 hover:from-lime-950 hover:border-lime-500 backdrop-blur-sm p-8 drop-shadow-2xl shadow-lime-600 rounded-xl transition-all ease-linear duration-150"
+              >
+                <h2 className="text-[2rem] font-bold pb-[1rem] leading-none title-style tracking-wide group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r group-hover:from-rose-500 group-hover:via-rose-500 group-hover:to-indigo-500 transition-all ease-linear duration-150">
+                  {servicesData.title}
+                </h2>
+                <h4 className="text-normal text-2xl text-zinc-400">
+                  {servicesData.subtitle}
+                </h4>
+                <h5 className="px-4 my-3 py-1 rounded-full bg-zinc-100/10 w-max text-yellow-500 font-semibold">
+                  {servicesData.pricing}
+                </h5>
+                <p className="text-zinc-400">{servicesData.desc}</p>
+                <div className="absolute right-5 bottom-5">
+                  <Image
+                    src={"/arrow.svg"}
+                    width={20}
+                    height={20}
+                    alt="star"
+                    className="group-hover:rotate-[45deg] transition-all ease-linear duration-150"
+                  />
                 </div>
-                <div className="grid grid-cols-3 w-[100%] max-[600px]:grid-cols-1 gap-5 ">
-                    {servicesData.map((servicesData) =>(
-                        <Link href={servicesData.url} key={servicesData.id} className="relative group flex flex-col bg-gradient-to-tl from-zinc-900 to-transparent border border-zinc-50/20 hover:from-sky-950 hover:border-sky-500 backdrop-blur-sm p-8 rounded-xl transition-all ease-linear duration-150">
-                            <h2 className="text-[2rem] font-bold pb-[1rem] leading-none title-style tracking-wide group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r group-hover:from-sky-500 group-hover:via-pink-500 group-hover:to-indigo-500 transition-all ease-linear duration-150">{servicesData.title}</h2>
-                            <h4 className="text-normal text-2xl text-zinc-400">{servicesData.subtitle}</h4>
-                            <h5 className="px-4 py-1 rounded-full bg-zinc-100/10 w-max text-yellow-500">{servicesData.pricing}</h5>
-                            <p className="text-zinc-400">{servicesData.desc}</p>
-                            <div className="absolute right-5 bottom-5" >
-                                <Image src={'/arrow.svg'} width={20} height={20} alt="star" className="group-hover:rotate-[45deg] transition-all ease-linear duration-150" />
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
+              </Link>
+            ))}
+          </div>
         </div>
-        </section>
-    )
+      </div>
+    </section>
+  );
 }
--- app/components/nav/NavMobile.tsx
+++ app/components/nav/NavMobile.tsx
@@ -1,66 +1,83 @@
-'use client'
-import React, {useState, useEffect} from 'react'
-import { NAV_LINK } from "@/app/constants/index"; 
-import Link from 'next/link'
- const HamburgerMenu = () => {
+"use client";
+import React, { useState, useEffect } from "react";
+import { NAV_LINK } from "@/app/constants/index";
+import Link from "next/link";
+const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const handleDrawer = () => {
     setIsOpen(!isOpen);
   };
-    return (
-        <div className='relative w-[30px] h-[30px] hidden max-[600px]:flexCenter'>
-            <input type="checkbox" name="" id="" onClick={handleDrawer} className='checkbox w-[30px] h-[30px] cursor-pointer z-10 opacity-[0]'/>
-            <div className="hamburger absolute right-0 flex flex-col justify-between w-[30px] h-[13px]">
-                <span className="line1 h-[2px] w-full bg-zinc-50 rounded-full"></span>
-                <span className="line2 h-[2px] w-full bg-zinc-50 rounded-full"></span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative w-[30px] h-[30px] hidden max-[600px]:flexCenter">
+      <input
+        type="checkbox"
+        name=""
+        id=""
+        onClick={handleDrawer}
+        className="checkbox w-[30px] h-[30px] cursor-pointer z-10 opacity-[0]"
+      />
+      <div className="hamburger absolute right-0 flex flex-col justify-between w-[30px] h-[13px]">
+        <span className="line1 h-[2px] w-full bg-zinc-50 rounded-full"></span>
+        <span className="line2 h-[2px] w-full bg-zinc-50 rounded-full"></span>
+      </div>
+    </div>
+  );
+};
 
-export const NavbarResp = () =>{
-    const [isOpen, setIsOpen] = useState(false);
+export const NavbarResp = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    const handleEscKeyPress = (e: React.KeyboardEvent | KeyboardEvent) => {
+      if (e.keyCode === 27 && isOpen) {
+        setIsOpen(false);
+      }
+    };
 
-    
-      useEffect(() => {
-        const handleEscKeyPress = (e: React.KeyboardEvent | KeyboardEvent) => {
-          if (e.keyCode === 27 && isOpen) {
-            setIsOpen(false);
-          }
-        };
-      
-    
-        if (isOpen) {
-          document.body.style.setProperty("overflow", "hidden");
-        } else {
-          document.body.style.removeProperty("overflow");
-        }
-    
-        document.addEventListener("keydown", handleEscKeyPress);
-    
-        return () => {
-          document.removeEventListener("keydown", handleEscKeyPress);
-        };
-      }, [isOpen]);
+    if (isOpen) {
+      document.body.style.setProperty("overflow", "hidden");
+    } else {
+      document.body.style.removeProperty("overflow");
+    }
 
-    return(
-        <nav className='absolute top-0 mx-auto w-full max-[600px]:fixed  z-50 '>
-          <div className="mx-auto flex items-center justify-between max-[600px]:w-full  max-w-screen-xl px-4 max-[600px]:py-4 0 max-[600px]:bg-white/10 max-[600px]:backdrop-blur-xl">
-          <Link href="#" className="title-style text-[1.2rem] max-[470px]:text-[1rem]">
+    document.addEventListener("keydown", handleEscKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscKeyPress);
+    };
+  }, [isOpen]);
+
+  return (
+    <nav className="absolute top-3 mx-auto w-full max-[600px]:fixed  z-50 ">
+      <div className="mx-auto flex items-center justify-between max-[600px]:w-full  max-w-screen-xl px-4 max-[600px]:py-4 0 max-[600px]:bg-white/10 max-[600px]:backdrop-blur-xl">
+        <div className="flex items-center">
+          <Link
+            href="#"
+            className="title-style text-[1.2rem] max-[470px]:text-[1rem]"
+          >
             Mango Labs
           </Link>
           <div className="hidden flex-nowrap max-[600px]:w-full lg:flex items-center transition-all h-full">
-            <div className=" lg:flex lg:gap-2 p-3">
-              {NAV_LINK.map((link) =>(
-                  <Link className="align-baseline font-medium hover:text-sky-500 transition-all duration-100 ease-in-out px-4 py-2 " href={link.href} key={link.key}>{link.label}</Link>
-                ))}
+            <div className=" lg:flex lg:gap-3 p-3">
+              {NAV_LINK.map((link) => (
+                <Link
+                  className="align-baseline font-medium text-slate-200 hover:text-sky-500 transition-all duration-100 ease-in-out "
+                  href={link.href}
+                  key={link.key}
+                >
+                  {link.label}
+                </Link>
+              ))}
             </div>
           </div>
-            <HamburgerMenu />
         </div>
-        </nav>
-    )
-}
+        <HamburgerMenu />
+        <Link className="px-4 py-1 bg-slate-600 rounded-full" href={"#"}>
+          (021) 55654432{" "}
+        </Link>
+      </div>
+    </nav>
+  );
+};
 export default NavbarResp;
 export { HamburgerMenu };
